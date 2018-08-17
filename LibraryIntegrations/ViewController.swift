3722eccf0e77225ac170447d56de480c801a39a5
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-08-17 15:17:51 -0500

    Initial Xcode project creation.

---
+++ LibraryIntegrations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LibraryIntegrations
+//
+//  Created by Tyler Milner on 8/17/18.
+//  Copyright © 2018 Bottle Rocket Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
